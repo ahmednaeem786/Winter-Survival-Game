cbf3f4bbd36c812e53d5cbf090d2be2c637ab5c4
Ahmed Naeem Bhuri <abhu0013@student.monash.edu>
2025-09-24 18:45:30 +0800

    feat(weapons): add Torch item exposing TorchAttackAction
    - Add Torch.java (game/items).
      - Item subclass for "Torch" that appears in actor inventories.
      - Implements allowableActions(owner, map) to generate TorchAttackAction(s) for adjacent targets that have the CAN_RECEIVE_STATUS capability.
      - Does NOT perform instanceof checks instead uses capability-based boundary (StatusAbilities.CAN_RECEIVE_STATUS).
      - Keeps item simple and focused i.e. only responsibility is presenting possible torch-based attack actions.

---
+++ src/game/items/Torch.java
@@ -0,0 +1,39 @@
+package game.items;
+
+import static game.capabilities.StatusAbilities.CAN_RECIEVE_STATUS;
+
+import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.items.Item;
+import edu.monash.fit2099.engine.positions.Exit;
+import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.positions.Location;
+import game.actors.GameActor;
+
+public class Torch extends Item {
+
+  public Torch() {
+    super("Torch", 'y', true);
+  }
+
+  @Override
+  public ActionList allowableActions(Actor owner, GameMap map) {
+    ActionList actions = new ActionList();
+    Location ownerLocation = map.locationOf(owner);
+
+    for (Exit exit : ownerLocation.getExits()) {
+      Location adjacent = exit.getDestination();
+
+      if (adjacent.containsAnActor()) {
+        Actor maybeTarget = adjacent.getActor();
+        if (maybeTarget.hasAbility(CAN_RECIEVE_STATUS)) {
+          GameActor gameTarget = (GameActor) maybeTarget;
+          actions.add(new TorchAttackAction(this, gameTarget, exit.getName()));
+        }
+      }
+    }
+
+    return actions;
+  }
+
+}
