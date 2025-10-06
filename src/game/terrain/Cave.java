cfbbc0383d9237cd97af96121a1ae7a7c67e3abe
rkaban <rrey0007@student.monash.edu>
2025-10-06 11:36:38 +0800

    feat: Implement CaveRule with every 5 turns spawning
    - Add Cave terrain class with spawn capabilities
    - Spawns animals every 5 turns regardless of probability
    - No special effects for cave-spawned animals
    - Map-specific species selection (Forest: Bear/Wolf/Deer, Plains: Bear/Wolf)
    - Part 5 of REQ2: Animal Spawning implementation

---
+++ src/game/terrain/Cave.java
@@ -0,0 +1,64 @@
+package game.terrain;
+
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
+import game.actors.Bear;
+import game.actors.Wolf;
+import game.actors.Deer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A class representing cave terrain that can spawn animals every 5 turns.
+ * @author REQ2 Implementation
+ */
+public class Cave extends Ground {
+    private static final int SPAWN_INTERVAL = 5; // Every 5 turns
+    private game.terrain.Snow.SpawnRule spawnRule;
+
+    public Cave() {
+        super('C', "Cave");
+        this.spawnRule = new CaveRule();
+    }
+
+    /**
+     * Cave spawn rule implementation.
+     * Spawns animals every 5 turns regardless of probability gates.
+     */
+    private static class CaveRule implements game.terrain.Snow.SpawnRule {
+        @Override
+        public boolean shouldAttemptSpawn(int globalTurn) {
+            return globalTurn % SPAWN_INTERVAL == 0;
+        }
+
+        @Override
+        public List<Class<? extends Actor>> allowedSpecies(GameMap map) {
+            // Use the map-specific spawn profile from Earth class
+            return game.Earth.getAllowedSpecies(map.toString(), Cave.class);
+        }
+
+        @Override
+        public void applySpawnEffects(Actor spawned, GameMap map) {
+            // Cave-spawned animals don't get special effects
+            // They spawn with normal characteristics
+        }
+    }
+
+    /**
+     * Called each turn to potentially spawn animals.
+     * @param location the location of this cave
+     */
+    @Override
+    public void tick(Location location) {
+        super.tick(location);
+        
+        // Get current turn from the global turn counter
+        int currentTurn = game.terrain.Snow.SpawnHelper.getGlobalTurn();
+        
+        // Attempt to spawn using the spawn helper
+        game.terrain.Snow.SpawnHelper.attemptSpawn(location, spawnRule, currentTurn);
+    }
+}
